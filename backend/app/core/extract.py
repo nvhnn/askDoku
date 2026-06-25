70e675ef7394b955762b69af36c57ac88322d182
dalo_t <164534632+kammini@users.noreply.github.com>
2026-06-25 21:26:39 +0200

    implement ocr

--- backend/app/core/extract.py
+++ backend/app/core/extract.py
@@ -1,16 +1,59 @@
 from pypdf import PdfReader
+from pypdfium2 import pdfium
+import pytesseract
 import docx
+
+MIN_CHARS = 50
+
+def render_page_to_image(filepath: str, page_number: int):
+    pdf = pdfium.PdfDocument(filepath)
+    page = pdf[page_number]
+    bitmap = page.render(scale=2.0)
+    return bitmap.to_pil()
+
+def ocr_page(filepath: str, page_number: int) -> str:
+    image = render_page_to_image(filepath, page_number)
+    return pytesseract.image_to_string(image)
+
+def ocr_embedded_images(page) -> list[str]:
+    results = []
+    for img in page.images:
+        pil_image = img.image
+        text = pytesseract.image_to_string(pil_image)
+        if text.strip():
+            results.append(text)
+    return results
+
 
 def extract_document(filepath: str) -> list[dict]:
     if filepath.endswith(".pdf"):
         reader = PdfReader(filepath)
-        return [
-            {
-                "page_number": i,
-                "content": page.extract_text().replace("\xad", "")
-            }
-            for i, page in enumerate(reader.pages, start=1)
-        ]
+        pages = []
+        for i, page in enumerate (reader.pages)
+            text = page.extract_text().strip()
+            images = page.images
+
+            if not images:
+                final_text = text if len(text) >= MIN_CHARS else ocr_page(filepath, i)
+            
+            elif len(text) < MIN_CHARS:
+                final_text = ocr_page(filepath, i)
+
+            else:
+                image_text = ocr_embedded_images(page)
+                final_text = text
+                for img_text in image_text:
+                    pages.append({
+                        "page_number": i + 1,
+                        "content": img_text
+                    })
+
+            pages.append({
+                "page_number": i + 1,
+                "content": final_text
+            })
+            
+        return pages
     
     elif filepath.endswith(".docx"):
         doc = docx.Document(filepath)
